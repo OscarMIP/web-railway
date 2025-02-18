b760bdf696ad6b1f6af785e40fb25c1da6ee336b
OscarMIP <oscar.mataro.epiaedu.cat>
2025-02-18 17:27:23 +0100

    arxius modificats

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import representativeImage from './assets/representativa.jpg';
+import representativeImage from './assets/descarga.jpg';
 
 function App() {
   const [count, setCount] = useState(0);
